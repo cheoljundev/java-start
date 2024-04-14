64f8e0ae8962390f3f6a3b688bb33576548cef27
cheoljundev <cheoljundev@gmail.com>
2024-04-14 14:08:40 +0900

    반복문 완료

---
+++ src/loop/ex/WhileEx2.java
@@ -0,0 +1,13 @@
+package loop.ex;
+
+public class WhileEx2 {
+    public static void main(String[] args) {
+        int i = 1;
+        int num = 2;
+        while(i <= 10){
+            System.out.println(num);
+            i++;
+            num+=2;
+        }
+    }
+}
