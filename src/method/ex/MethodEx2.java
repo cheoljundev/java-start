fd93b6490bedb3631c4021f3271d4e17ae071201
cheoljundev <cheoljundev@gmail.com>
2024-04-15 14:29:55 +0900

    메서드 완료

---
+++ src/method/ex/MethodEx2.java
@@ -0,0 +1,16 @@
+package method.ex;
+
+public class MethodEx2 {
+    public static void main(String[] args) {
+        String message = "Hello, world!";
+        printMsg(3, message);
+        printMsg(5, message);
+        printMsg(7, message);
+    }
+
+    public static void printMsg(int n, String msg) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(msg);
+        }
+    }
+}
