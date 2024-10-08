b9679de6fcf58cacee6c632fc1597e15df7a16db
cheoljundev <cheoljundev@gmail.com>
2024-10-09 00:12:39 +0900

    변수 선언과 초기화

---
+++ src/two/variable/Var6.java
@@ -0,0 +1,8 @@
+package two.variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        // System.out.println("a = " + a); // 초기화하지 않으면 컴파일 에러가 발생.
+    }
+}
