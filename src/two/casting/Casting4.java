0f9479b3a05e7d74982456109265e4fd89fc45c8
cheoljundev <cheoljundev@gmail.com>
2024-10-18 10:16:23 +0900

    계산과 형변환

---
+++ src/two/casting/Casting4.java
@@ -0,0 +1,19 @@
+package two.casting;
+
+public class Casting4 {
+    public static void main(String[] args) {
+        int div1 = 3 / 2; // 1.5여야 한다.
+        System.out.println("div1 = " + div1); // 1
+
+        double div2 = 3 / 2;
+        System.out.println("div2 = " + div2); // 1.0
+
+        double div3 = (double) 3 / 2;
+        System.out.println("div3 = " + div3); // 1.5
+
+        int a = 3;
+        int b = 2;
+        double result = (double) a / b;
+        System.out.println("result = " + result); // 1.5
+    }
+}
