dd0bbb113ec54bb28871a6bb0c4898e6ed41298d
cheoljundev <cheoljundev@gmail.com>
2024-10-23 00:20:07 +0900

    메서드 시작

---
+++ src/two/method/Method1.java
@@ -0,0 +1,19 @@
+package two.method;
+
+public class Method1 {
+    public static void main(String[] args) {
+        // 계산1
+        int a = 1;
+        int b = 2;
+        System.out.println(a + " + " + b + " 연산 수행");
+        int sum1 = a + b;
+        System.out.println("결과 1 출력 : " + sum1);
+
+        // 계산2
+        int x = 20;
+        int y = 30;
+        System.out.println(x + " + " + y + " 연산 수행");
+        int sum2 = x + y;
+        System.out.println("결과 2 출력 : " + sum2);
+    }
+}
