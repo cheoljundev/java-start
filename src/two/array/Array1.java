05c72daaf065435462a756ff2019af0bc2d1ac58
cheoljundev <cheoljundev@gmail.com>
2024-10-22 11:37:54 +0900

    배열 시작

---
+++ src/two/array/Array1.java
@@ -0,0 +1,19 @@
+package two.array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int student1 = 90;
+        int student2 = 80;
+        int student3 = 50;
+        int student4 = 30;
+        int student5 = 50;
+        //  변수 이름에는 반복문을 적용할 수 없다.
+
+        System.out.println("학생 1 점수 : " + student1);
+        System.out.println("학생 2 점수 : " + student2);
+        System.out.println("학생 3 점수 : " + student3);
+        System.out.println("학생 4 점수 : " + student4);
+        System.out.println("학생 5 점수 : " + student5);
+
+    }
+}
