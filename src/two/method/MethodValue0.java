301ea1615e61fec97846f8465a01f3e4caa80fee
cheoljundev <cheoljundev@gmail.com>
2024-10-23 14:16:46 +0900

    메서드 호출과 값 전달1

---
+++ src/two/method/MethodValue0.java
@@ -0,0 +1,12 @@
+package two.method;
+
+public class MethodValue0 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = num1;
+        num2 = 20;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
+}
